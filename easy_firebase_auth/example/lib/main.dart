33d6fdbd3f592b2372311ff9791f7c30fe1689ef
Chetan <awesome11activity@gmail.com>
2020-09-08 13:54:10 +0530

    Progress

--- easy_firebase_auth/example/lib/main.dart
+++ easy_firebase_auth/example/lib/main.dart
@@ -1,78 +1,91 @@
-import 'package:allsirsa/Methods/email.dart';
-import 'package:allsirsa/Methods/phone.dart';
-import 'package:allsirsa/Methods/send.email.dart';
-import 'package:allsirsa/infrastructure/uiutils.dart';
-import 'package:allsirsa/infrastructure/utils.dart';
-import 'package:allsirsa/screens/signin.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_firebase_auth/easy_firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebase/firebase.dart';
-import 'Methods/google.dart';
-
 import 'package:firebase_core/firebase_core.dart';
-import 'package:device_preview/device_preview.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => MyApp(),
-    ),
-  );
+  runApp(MyApp()
+      // DevicePreview(
+      //   enabled: true,
+      //   builder: (context) => MyApp(),
+      // ),
+      );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth.instance.signOut();
+    // .signInWithEmailAndPassword(
+    //     email: 'chetansirsa@gmail.com', password: '12345678')
+    // .then((value) => print(value));
+    // logStore();
+
     return MaterialApp(
-      title: 'Easy Auth',
-      debugShowCheckedModeBanner: false,
-      home: SignIn(
-        themeColor: Colors.deepPurple,
-        methods: [
-          Google(),
-          Email(config: EmailConfig(emailVerificationRequired: false))
-        ],
-        auth: FirebaseAuth.instance,
-        onSuccess: (user, context, fields) {
-          showSnackBar(
-              'You made the signUp flow in record time. Cheers.', context);
-          print('user: $user \n fields: $fields');
-        },
-      ),
-    );
+        title: 'Easy Auth',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(body: Aaa()));
   }
 }
 
-/**
- * Makes sure that onUnAutheticated access see the Widget
- *  [onAuthenticated] Widget to be protected
- */
-Widget protect(
-    {@required Function0<Widget> onAuthenticated,
-    Function0<Widget> onUnAutheticated,
-    FirebaseAuth auth}) {
-  auth ??= FirebaseAuth.instance;
-  onUnAutheticated ??= () {
-    return SignIn(
-      themeColor: Colors.deepPurple,
-      methods: [Email(config: EmailConfig(emailVerificationRequired: false))],
-      auth: FirebaseAuth.instance,
-      onSuccess: (user, context, fields) {
-        protect(
-          onAuthenticated: onAuthenticated,
-          auth: auth,
-        );
-      },
-    );
-  };
+class Aaa extends StatefulWidget {
+  const Aaa({
+    Key key,
+    // @required this.protect2,
+  }) : super(key: key);
 
-  if (isNull(auth.currentUser)) {
-    return onUnAutheticated();
-  } else {
-    return onAuthenticated();
+  @override
+  _AaaState createState() => _AaaState();
+}
+
+class _AaaState extends State<Aaa> {
+  @override
+  Widget build(BuildContext context) {
+    // final protect2 = ;
+    // print((protect2.onAuthenticated as Scaffold).appBar);
+    print('object');
+    return Center(
+        child: Container(
+            child: Protection(
+                auth: FirebaseAuth.instance,
+                // key: UniqueKey(),
+                onAuthenticated: Scaffold(
+                    appBar: AppBar(
+                  title: Text('Assssdd!!!'),
+                )))));
   }
 }
+
+// /**
+//  * Makes sure that onUnAutheticated access see the Widget
+//  *  [onAuthenticated] Widget to be protected
+//  */
+//(
+//     {@required Function0<Widget> onAuthenticated,
+//     Function0<Widget> onUnAutheticated,
+//     FirebaseAuth auth}) {
+//   auth ??= FirebaseAuth.instance;
+//   onUnAutheticated ??= () {
+//     return SignIn(
+//       themeColor: Colors.deepPurple,
+//       methods: [Email(config: EmailConfig(emailVerificationRequired: false))],
+//       auth: FirebaseAuth.instance,
+//       onSuccess: (user, context, fields) {
+//         protect(
+//           onAuthenticated: onAuthenticated,
+//           auth: auth,
+//         );
+//       },
+//     );
+//   };
+
+//   if (isNull(auth.currentUser)) {
+//     return onUnAutheticated();
+//   } else {
+//     return onAuthenticated();
+//   }
+// }
