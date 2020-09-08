c9511982a3bd1f0201e2ccb27ec1407990781000
Chetan <awesome11activity@gmail.com>
2020-09-08 11:59:11 +0530

    Some Progress

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 // import 'package:firebase/firebase.dart';
 import 'Methods/google.dart';
 
-import 'home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:device_preview/device_preview.dart';
 
